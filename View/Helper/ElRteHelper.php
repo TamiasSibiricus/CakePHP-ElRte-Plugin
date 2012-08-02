7cb68159f35bea7d3118302da8ec91e0d1e92bea
Denys Kyselov <denys.kyselov@cloudspoint.com>
2012-08-02 03:16:55 +0300

    Fix for multiple instances of editor

--- View/Helper/ElRteHelper.php
+++ View/Helper/ElRteHelper.php
@@ -24,6 +24,7 @@ class ElRteHelper extends Helper
 		//'absoluteURLs' => 'false',
 		'toolbar' => 'complete'
 	);
+	public $fields_count = 0;
 
 	public function __construct(View $View, $options = array()) {
 		parent::__construct($View, $options);
@@ -35,59 +36,65 @@ class ElRteHelper extends Helper
 		$this->configs['sitebaseURL'] = 'http://'.$_SERVER['SERVER_NAME'].Router::url('/');
 	}
 
-    public function render($fieldName, $options = array()){
-
-        //load editor
-	    $this->Html->css("ElRte.elrte.min", null, array('inline' => false));
-        $this->Html->script("ElRte.elrte.full", array('inline' => false));
-	    $this->Html->script("ElRte.i18n/elrte.".$this->configs['lang'], array('inline' => false));
+	public function afterRender(){
+		if (!empty($this->fields_count)){
+		//load editor
+		$this->Html->css("ElRte.elrte.min", null, array('inline' => false));
+		$this->Html->script("ElRte.elrte.full", array('inline' => false));
+		$this->Html->script("ElRte.i18n/elrte.".$this->configs['lang'], array('inline' => false));
 
 		//load finder
-	    $this->Html->css("ElRte.elfinder.min", null, array('inline' => false));
-        $this->Html->script("ElRte.elfinder.min", array('inline' => false));
-	    //$this->Html->script("ElRte.i18n/elfinder.".$this->configs['lang'], array('inline' => false));
+		$this->Html->css("ElRte.elfinder.min", null, array('inline' => false));
+	        $this->Html->script("ElRte.elfinder.min", array('inline' => false));
+		//$this->Html->script("ElRte.i18n/elfinder.".$this->configs['lang'], array('inline' => false));
 
-	    $this->Form->setEntity($fieldName);
-	    $id = ucfirst($this->Form->model()).ucfirst($this->Form->field());
+		$js = "
+		    //var dialog;
+	            elRTE.prototype.options.panels.simple = [
+	                'bold', 'italic', 'underline', 'justifyleft', 'justifyright',
+	                'justifycenter', 'justifyfull', 'formatblock', 'insertorderedlist', 'insertunorderedlist',
+		    ];
+	            elRTE.prototype.options.toolbars.simple = ['simple'];
+	        ";
+		$this->Html->scriptBlock($js, array("inline"=> false));
 
-	    $options = array_merge($this->configs, $options);
+		}
+	}
 
-	    $lines = '';
-	    foreach ($options as $option => $value) {
-	  		$lines .= $option . " : '" . $value . "',\n";
-	  	}
-	    //Standard ugly finder init
-	    // use it if modern way don't work
-		//$lines .= "
-        //    fmOpen : function(callback) {
-        //        $('<div id=\"myelfinder\" />').elfinder({
-        //            url : '".$this->Html->url(array( "plugin" => "el_rte", "controller"=>"el_finder", "action"=>"finder"))."',
-        //            lang : 'en',
-        //            dialog : { width : 800, modal : true, title : 'Select an Image' }, // open in dialog window
-        //            closeOnEditorCallback : true, // close after file select
-        //            editorCallback : callback
-        //        });
-        //    }\n
-		//";
+    public function render($fieldName, $options = array()){
 
-	    //modern way
-	    //https://github.com/Studio-42/elFinder/wiki/Integration-with-elRTE-1.x
-	    $lines .= "
-	        absoluteURLs : false,
-            cssfiles : ['http://".$_SERVER['SERVER_NAME'].Router::url('/')."el_rte/css/elrte-inner.css'],
+	    //$this->Form->setEntity($fieldName);
+	    //$id = ucfirst($this->Form->model()).ucfirst($this->Form->field());
+	    $id = $this->domId($fieldName);
+	    if (!strpos($fieldName, ".")){
+		    $id = ucfirst($this->Form->model()).ucfirst($id);
+	    }
+
+		$options = array_merge($this->configs, $options);
+
+		$lines = '';
+		foreach ($options as $option => $value) {
+		    $lines .= $option . " : '" . $value . "',\n";
+		}
+
+		//modern way
+		//https://github.com/Studio-42/elFinder/wiki/Integration-with-elRTE-1.x
+		$lines .= "
+			absoluteURLs : false,
+	        cssfiles : ['".Router::url('/', true)."el_rte/css/elrte-inner.css'],
 			fmOpen: function(callback) {
 				if (!dialog) {
 					// create new elFinder
 					dialog = $('<div />').dialogelfinder({
-                        url : '".$this->Html->url(array( "plugin" => "el_rte", "controller"=>"el_finder", "action"=>"finder"))."',
+	                    url : '".$this->Html->url(array( "plugin" => "el_rte", "controller"=>"el_finder", "action"=>"finder"))."',
 	                    lang : 'en',
 	                    dialog : { width : 800, modal : true, title : 'Select an Image' }, // open in dialog window
 						commandsOptions: {
 							getfile: {
-								oncomplete : 'close' // close/hide elFinder
-							}
-						},
-						getFileCallback: callback // pass callback to file manager
+							oncomplete : 'close' // close/hide elFinder
+						}
+					},
+					getFileCallback: callback // pass callback to file manager
 						//getFileCallback: function(file) { callback(file.path); } // pass callback to file manager
 					});
 				} else {
@@ -96,19 +103,13 @@ class ElRteHelper extends Helper
 				}
 			}\n
 		";
-
+	$opts = "{ ".$lines." }";
         $js = "
-            var dialog;
-            elRTE.prototype.options.panels.simple = [
-			     'bold', 'italic', 'underline', 'justifyleft', 'justifyright',
-			     'justifycenter', 'justifyfull', 'formatblock', 'insertorderedlist', 'insertunorderedlist',
-			];
-            elRTE.prototype.options.toolbars.simple = ['simple'];
             $(document).ready(function(){
-				var opts = { ".$lines." }
-				$('#".$id."').elrte(opts);
+				$('#".$id."').elrte(".$opts.");
             });
         ";
-        return $this->Html->scriptBlock($js, array("inline"=> false));
+		$this->fields_count = $this->fields_count + 1;
+	    $this->Html->scriptBlock($js, array("inline"=> false));
     }  
 }
